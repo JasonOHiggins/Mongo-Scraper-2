971b52de3bab08871ef76e4c9a960ad8c2d225d3
Jason O'Higgins <jason.ohiggins@gmail.com>
2018-07-30 17:45:48 -0500

    scripts / controllers
    created date and scrape scripts and updated headline and note controller files with get, save, delete functions

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,16 +1,30 @@
-var mongoose = require("mongoose");
 
-var Schema = mongoose.Schema;
 
-var noteSchema = new Schema({
-    _headlineId: {
-        type: Schema.Types.ObjectId,
-        ref: "Headline"
+
+var Note = require("../models/Note");
+var makeDate = require("../scripts/date");
+
+module.exports = {
+    get: function(data, cb) {
+        Note.find({
+            _headlineId: data._headlineId
+        }, cb);
     },
-    date: String,
-    noteText: String
-}),
+    save: function(data, cb) {
+        var newNote = {
+            _headlineId: data._id,
+            data: madeDate(),
+            noteText: data.noteText
+        };
 
-var Note = mongoose.model("Note", noteSchema);
-
-module.exports = Note;
+        Note.create(newNote, function (err, doc) {
+            if (err) {
+                console.log(err);
+            }
+            else{
+                console.log(doc);
+                cb(doc);
+            }
+        });
+    },
+}
